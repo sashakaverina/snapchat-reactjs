1baaee32321fe454e9c8b52193f335eb3d882e2e
Sasha Kaverina <sasha.kaverina@lewagon.org>
2022-02-07 19:54:46 +0900

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCZWShFp723SpiT8brH3nO1mVn-zexR7oE",
+    authDomain: "snapchat-react-ec320.firebaseapp.com",
+    projectId: "snapchat-react-ec320",
+    storageBucket: "snapchat-react-ec320.appspot.com",
+    messagingSenderId: "94095756020",
+    appId: "1:94095756020:web:933a53102005e015696ca1"
+  };
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
