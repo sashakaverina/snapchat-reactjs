7eb62afdb5cd8fe742fde349476059635e49be73
Sasha Kaverina <sasha.kaverina@lewagon.org>
2022-02-13 22:14:03 +0900

    icons

--- src/Preview.js
+++ src/Preview.js
@@ -3,7 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { resetCameraImage, selectCamera } from './features/cameraSlice';
 import './Preview.css';
-import { Close } from '@material-ui/icons';
+import { AttachFile, Close, Create, Crop, MusicNote, Note, Send, Timer } from '@material-ui/icons';
+import { TextField } from '@material-ui/core';
 
 function Preview() {
     const cameraImage = useSelector(selectCamera);
@@ -26,7 +27,21 @@ function Preview() {
   return (
     <div className='preview'>
         <Close onClick={closePreview} className='preview__close' />
+        <div className='preview__toolbarRight'>
+          <Create/>
+          <Note/>
+          <MusicNote/>
+          <AttachFile/>
+          <Crop/>
+          <Timer/>
+
+        </div>
+
         <img src={cameraImage} alt=''/>
+        <div className='preview__footer'>
+          <h2>Send Now</h2>
+          <Send className='preview__sendIcon' fontSize='small'/>
+        </div>
     </div>
   )
 }
